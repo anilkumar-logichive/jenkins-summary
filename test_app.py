2bb7cf5f3426063a2e71148dc53d697692a144b5
anilkumar-logichive <58001796+anilkumar-logichive@users.noreply.github.com>
2022-11-28 17:42:47 +0530

    updates g

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from logic import check_posted_data
 
 
 class TestSnippet(unittest.TestCase):
-    @unittest.skip("demonstrating skipping")
+
     def test_check_posted_data_first_set(self):
         # testing the check_posted_data method with different input sets
 
@@ -22,7 +22,7 @@ class TestSnippet(unittest.TestCase):
 
 
 class TestSnippet1(unittest.TestCase):
-    @unittest.skip("demonstrating skipping")
+    
     def test_check_posted_data_second_set(self):
         print(f"Testing function - {check_posted_data.__name__} Set 2")
         data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
@@ -37,7 +37,7 @@ class TestSnippet1(unittest.TestCase):
 
 
 class TestSnippet2(unittest.TestCase):
-    @unittest.skip("demonstrating skipping")
+
     def test_check_posted_data_third_set(self):
 
         print(f"Testing function - {check_posted_data.__name__} Set 3")
@@ -51,7 +51,7 @@ class TestSnippet2(unittest.TestCase):
 
             print(f"Test data - {data}")
 
-    @unittest.skip("demonstrating skipping")
+
     def test_check_posted_data_fourth_set(self):
         print(f"Testing function - {check_posted_data.__name__} Set 4")
         data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
