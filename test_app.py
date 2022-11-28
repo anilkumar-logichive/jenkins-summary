da93d3383c021dcd90ff57314cadad9938e80b35
anilkumar-logichive <58001796+anilkumar-logichive@users.noreply.github.com>
2022-11-28 16:58:55 +0530

    .ignore

--- test_app.py
+++ test_app.py
@@ -22,6 +22,8 @@ class TestSnippet(unittest.TestCase):
 
             print(f"Test data - {data}")
 
+
+class TestSnippet1(unittest.TestCase):
     def test_check_posted_data_second_set(self):
         print(f"Testing function - {check_posted_data.__name__} Set 2")
         data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
@@ -34,6 +36,8 @@ class TestSnippet(unittest.TestCase):
 
             print(f"Test data - {data}")
 
+
+class TestSnippet2(unittest.TestCase):
     def test_check_posted_data_third_set(self):
 
         print(f"Testing function - {check_posted_data.__name__} Set 3")
@@ -59,6 +63,8 @@ class TestSnippet(unittest.TestCase):
 
             self.x = print(f"Test data - {data}")
 
+
+class TestSnippet4(unittest.TestCase):
     def test_check_posted_data_fifth_set(self):
         print(f"Testing function - {check_posted_data.__name__} Set 5")
         data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
