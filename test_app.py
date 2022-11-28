f5e47dfd2d86fa6b71f08fd4293a667df3e62edf
anilkumar-logichive <58001796+anilkumar-logichive@users.noreply.github.com>
2022-11-28 12:09:09 +0530

    updated test cases

--- test_app.py
+++ test_app.py
@@ -57,17 +57,17 @@ class TestSnippet(unittest.TestCase):
         #     print(f"Test data - {data}")
 
     # @pytest.mark.skip(reason="no way of currently testing this")
-    def test_check_posted_data_fourth_set(self):
-        print(f"Testing function - {check_posted_data.__name__} Set 2")
-        data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
-                                                                                                 301]]
-
-        for data in data_list:
-            result = check_posted_data(data[0], data[1])
-
-            self.assertEqual(result, data[2])
-
-            print(f"Test data - {data}")
+    # def test_check_posted_data_fourth_set(self):
+    #     print(f"Testing function - {check_posted_data.__name__} Set 2")
+    #     data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
+    #                                                                                              301]]
+    #
+    #     for data in data_list:
+    #         result = check_posted_data(data[0], data[1])
+    #
+    #         self.assertEqual(result, data[2])
+    #
+    #         print(f"Test data - {data}")
         # print(f"Testing function - {check_posted_data.__name__} Set 2")
         # data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
         #                                                                                          301]]
@@ -80,17 +80,17 @@ class TestSnippet(unittest.TestCase):
         #     print(f"Test data - {data}")
 
     # @pytest.mark.skip(reason="no way of currently testing this")
-    def test_check_posted_data_fifth_set(self):
-        print(f"Testing function - {check_posted_data.__name__} Set 2")
-        data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
-                                                                                                 301]]
-
-        for data in data_list:
-            result = check_posted_data(data[0], data[1])
-
-            self.assertEqual(result, data[2])
-
-            print(f"Test data - {data}")
+    # def test_check_posted_data_fifth_set(self):
+    #     print(f"Testing function - {check_posted_data.__name__} Set 2")
+    #     data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
+    #                                                                                              301]]
+    #
+    #     for data in data_list:
+    #         result = check_posted_data(data[0], data[1])
+    #
+    #         self.assertEqual(result, data[2])
+    #
+    #         print(f"Test data - {data}")
         # print(f"Testing function - {check_posted_data.__name__} Set 2")
         # data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
         #                                                                                          301]]
@@ -105,5 +105,4 @@ class TestSnippet(unittest.TestCase):
 
 if __name__ == "__main__":
     import xmlrunner
-
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test_reports'))
