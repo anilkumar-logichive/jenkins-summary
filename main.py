0c01f72cd0b461aaa760998c0811283527d164c8
anilkumar-logichive <58001796+anilkumar-logichive@users.noreply.github.com>
2023-01-10 20:30:05 +0530

    Create main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def fetch_build_info():
         s3_client.upload_file(file_path, bucket_name,
                               f"reports/{job_name}/lastBuildNumber/meta.json", ExtraArgs={'ACL': 'public-read'})
 
-        response = requests.get(f"{jenkins_url}/job/{job_name}/{build_number}/testReport/api/json",
+        response = requests.get(f"{jenkins_url}job/{job_name}/{build_number}/testReport/api/json",
                                 auth=HTTPBasicAuth(username, token))
         print("*****************************************")
         print(response.text)
