2fb9f6faa173d5ee29ad8f0c9260eb9b7baa4a53
anilkumar-logichive <58001796+anilkumar-logichive@users.noreply.github.com>
2023-01-10 19:38:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,19 +20,6 @@ bucket_name = sys.argv[6]
 access_key = sys.argv[7]
 secret_key = sys.argv[8]
 
-session = boto3.Session( 
-         aws_access_key_id=access_key, 
-         aws_secret_access_key=secret_key)
-
-
-#Then use the session to get the resource
-s3 = session.resource('s3')
-
-my_bucket = s3.Bucket('logichivebuildreport')
-
-for my_bucket_object in my_bucket.objects.all():
-    print(my_bucket_object.key)
-
 s3_client = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 
 
